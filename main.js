fd739a797c73cfe57621f25099a2663fcb6a3209
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-10 01:36:31 -0500

    Add click events for the 3 boxes

---
+++ main.js
@@ -0,0 +1,25 @@
+// User hovers over one of the boxes
+
+
+// Click on box 1 message
+document.getElementById("box-1").addEventListener("click",
+		function() {
+			alert("Oooh - so close, but no cigar");
+		}, false
+);
+
+// Click on box 2 message
+document.getElementById("box-2").addEventListener("click",
+		function() {
+			alert("DING DING DING - We have a winner");
+		}, false
+);
+
+// Click on box 3 message
+document.getElementById("box-3").addEventListener("click",
+		function() {
+			alert("Ooops, butter luck next time");
+		}, false
+);
+
+// click any box and get href to appear
